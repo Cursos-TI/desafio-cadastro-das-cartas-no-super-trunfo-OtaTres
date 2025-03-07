1d44cc0ff90782663112faf2c490039d5e89c6dd
OtaTres <otaviosilvatres@gmail.com>
2025-03-07 02:47:29 +0000

    CartasSuperTrunfo 2.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -30,7 +30,7 @@ printf("Digite o numero de Pontos Turísticos: \n", turismo01);
 printf("Digite o PIB: \n", pib01);
   scanf(" %f", &pib01);
 
-printf("Digite a Area em Km² \n", area01);
+printf("Digite a Area em Km²: \n", area01);
   scanf(" %f", &area01);
   printf("\n");
 
@@ -39,10 +39,21 @@ printf("O nome do Estado é: %s \n"
        "O Codigo da Cidade é: %s \n"
        "O numero de Habitantes é: %d \n"
        "O numero de Pontos Turisticos são: %d \n"
-       "O PIB dessa cidade é: %f \n"
-       "A Area em Km² é: %f \n"
+       "O PIB dessa cidade é: %.2f \n"
+       "A Area em Km² é: %.2f \n"
        
        ,estado01, cidade01, cod01, populacao01, turismo01, pib01, area01);
+printf("\n");
+
+//////////// divisoes carta 1 /////////////
+
+float denspop01 = (float)(populacao01 / area01);
+printf("A densidade populacional é: %.2f hab/km²\n", denspop01);
+
+pib01 *= 1e9;
+
+float capita01 = (pib01 / populacao01);
+printf("O PIB per capita é: %.2f Reais\n", capita01);
 
 //////////// carta 2 ///////////
 
@@ -81,6 +92,17 @@ printf("O nome do Estado é: %s \n"
     "A Area em Km² é: %f \n"
 
     ,estado02, cidade02, cod02, populacao02, turismo02, pib02, area02);
+printf("\n");
+
+///////////// divisoes carta 2 /////////////
+
+float denspop02 = (float)(populacao02 / area02);
+printf("A densidade populacional é: %.2f hab/km²\n", denspop02);
+
+pib02 *= 1e9;
+
+float capita02 = (pib02 / populacao02);
+printf("O PIB per capita é: %.2f Reais\n", capita02);
 
 return 0;
 }
